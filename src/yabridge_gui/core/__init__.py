dccad868ebc4ae3484ef9c50d38066e87fdf42a6
Andrianos Papamarkou <papamarkoua@gmail.com>
2026-09-03 23:37:01 +0300

    Add comment headers

--- src/yabridge_gui/core/__init__.py
+++ src/yabridge_gui/core/__init__.py
@@ -1,3 +1,19 @@
+# __   __    _          _     _               ____ _   _ ___
+# \ \ / /_ _| |__  _ __(_) __| | __ _  ___   / ___| | | |_ _|
+#  \ V / _` | '_ \| '__| |/ _` |/ _` |/ _ \ | |  _| | | || |
+#   | | (_| | |_) | |  | | (_| | (_| |  __/ | |_| | |_| || |
+#   |_|\__,_|_.__/|_|  |_|\__,_|\__, |\___|  \____|\___/|___|
+#                               |___/
+#   ____            _             _ _
+#  / ___|___  _ __ | |_ _ __ ___ | | | ___ _ __
+# | |   / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+# | |__| (_) | | | | |_| | | (_) | | |  __/ |
+#  \____\___/|_| |_|\__|_|  \___/|_|_|\___|_|
+#
+# Author: Andrianos Papamarkou
+# Licence: GPL3
+# https://github.com/apapamarkou/yabridge-gui-controller
+
 from yabridge_gui.core.distro import Distribution, detect_distribution
 from yabridge_gui.core.environment import CheckStatus, EnvironmentCheck, run_environment_checks
 from yabridge_gui.core.system import get_architecture, get_kernel
